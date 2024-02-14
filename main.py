69adf94a2c48e42e157e97fa9c18fbcf7ccdd0f7
yarikpython <kozoderovaroslav@gmail.com>
2024-02-14 20:30:21 +0100

    New files

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("hello")
+class Toy:
+    def __init__(self, name, price, category, amount):
+        self.name = name
+        self.price = price
+        self.category = category
+        self.amount = amount
+        
