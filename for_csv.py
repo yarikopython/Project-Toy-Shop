9d10c5f28bb2869345c418f99443d27bd3f49c04
yarikpython <kozoderovaroslav@gmail.com>
2024-02-14 21:58:06 +0100

    This file get updated

--- for_csv.py
+++ for_csv.py
@@ -1,2 +1,21 @@
-import models
+from models import Toy
 import csv
+objectik = Toy(input("Write a name of Toy: "),
+               int(input("Write a price of Toy: ")),
+               input("Write category of Toy: "), input("Write a amount of Toy: "))
+listik = [
+    objectik.name,
+    objectik.price,
+    objectik.amount,
+    objectik.category
+]
+
+with open("csv.csv", "w", newline="") as writer:
+    spammwriter = csv.writer(writer, delimiter="\n")
+    spammwriter.writerow(listik)
+with open("csv.csv", "r") as reader:
+    spammreader= csv.reader(reader)
+    for x in spammreader:
+        print(x)
+
+    
