d364ece24be204493f2835e51c209412cb9d986a
yarikpython <kozoderovaroslav@gmail.com>
2024-02-14 20:37:59 +0100

    New file for work with csv file

--- main.py
+++
@@ -1,7 +0,0 @@
-class Toy:
-    def __init__(self, name, price, category, amount):
-        self.name = name
-        self.price = price
-        self.category = category
-        self.amount = amount
-        
---
+++ models.py
@@ -0,0 +1,7 @@
+class Toy:
+    def __init__(self, name, price, category, amount):
+        self.name = name
+        self.price = price
+        self.category = category
+        self.amount = amount
+        
