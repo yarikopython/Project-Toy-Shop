4e20ad2dbc4c0f9832da0bdcde28b3c0101f8d38
yarikpython <kozoderovaroslav@gmail.com>
2024-02-18 15:23:37 +0100

    For work with file csv.csv

--- for_csv.py
+++ for_csv.py
@@ -3,19 +3,35 @@ import csv
 objectik = Toy(input("Write a name of Toy: "),
                int(input("Write a price of Toy: ")),
                input("Write category of Toy: "), input("Write a amount of Toy: "))
-listik = [
-    objectik.name,
-    objectik.price,
-    objectik.amount,
-    objectik.category
-]
-
-with open("csv.csv", "w", newline="") as writer:
-    spammwriter = csv.writer(writer, delimiter="\n")
-    spammwriter.writerow(listik)
-with open("csv.csv", "r") as reader:
-    spammreader= csv.reader(reader)
-    for x in spammreader:
-        print(x)
+listik = {
+    "name":objectik.name,
+    "price":objectik.price,
+    "amount":objectik.amount,
+    "category":objectik.category
+}
 
     
+
+def spammwriter():
+    with open("csv.csv", "w", newline="") as writer:
+        spammwriter = csv.writer(writer, delimiter="\n")
+        spammwriter.writerow(f"Keys: {listik.keys}, Value: {listik.values}")
+
+def spammreader():
+    with open("csv.csv", "r") as reader:
+        spammreader= csv.reader(reader)
+        for x in spammreader:
+            print(x)
+try:
+    while True:
+        print("[1] - Write in csv ")
+        inputik = int(input("What do you want to choose?: "))
+        match inputik:
+            case 1:
+                spammwriter()
+            case 2:
+                spammreader()
+            case 3:
+                break
+except FileNotFoundError:
+    print("File is didnt founded")          
